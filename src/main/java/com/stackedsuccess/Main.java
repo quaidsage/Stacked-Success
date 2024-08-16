42ca80473ec8407fb39808671c18a93d248f6ac1
axu732 <axu732@aucklanduni.ac.nz>
2024-08-16 19:49:35 +1200

    Made game board fit the agreed upon scene size

--- src/main/java/com/stackedsuccess/Main.java
+++ src/main/java/com/stackedsuccess/Main.java
@@ -1,9 +1,8 @@
 package com.stackedsuccess;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.stackedsuccess.controllers.GameScreenController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,49 +11,53 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public enum SceneName {
-        MENU
-    }
+  public enum SceneName {
+    MENU,
+    GAMEBOARD
+  }
 
-    private static Map<SceneName, Parent> scenes = new HashMap<>();
+  private static Map<SceneName, Parent> scenes = new HashMap<>();
 
-    private static Scene scene;
+  private static Scene scene;
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
 
-    /**
-     * Returns the node associated to the input file. The method expects that the file is located in
-     * "src/main/resources/fxml".
-     *
-     * @param fxml The name of the FXML file (without extension).
-     * @return The node of the input file.
-     * @throws IOException If the file is not found.
-     */
-    public static Parent loadFxml(final String fxml) throws IOException {
-        return new FXMLLoader(Main.class.getResource("/fxml/" + fxml + ".fxml")).load();
-    }
+  /**
+   * Returns the node associated to the input file. The method expects that the file is located in
+   * "src/main/resources/fxml".
+   *
+   * @param fxml The name of the FXML file (without extension).
+   * @return The node of the input file.
+   * @throws IOException If the file is not found.
+   */
+  public static Parent loadFxml(final String fxml) throws IOException {
+    return new FXMLLoader(Main.class.getResource("/fxml/" + fxml + ".fxml")).load();
+  }
 
-    /**
-     * This method is invoked when the application starts. It loads and shows the "Canvas" scene.
-     *
-     * @param stage The primary stage of the application.
-     * @throws IOException If "src/main/resources/fxml/canvas.fxml" is not found.
-     */
-    @Override
-    public void start(final Stage stage) throws IOException {
-        scenes.put(SceneName.MENU, loadFxml("GameScreen"));
-        Parent root = scenes.get(SceneName.MENU);
-        scene = new Scene(root, 1400, 800);
-        stage.setScene(scene);
-        stage.setTitle("Stacked Success");
-        stage.show();
-        root.requestFocus();
+  /**
+   * This method is invoked when the application starts. It loads and shows the "Canvas" scene.
+   *
+   * @param stage The primary stage of the application.
+   * @throws IOException If "src/main/resources/fxml/canvas.fxml" is not found.
+   */
+  @Override
+  public void start(final Stage stage) throws IOException {
 
-        stage.setOnCloseRequest(event -> {
-            System.exit(0);
+    scenes.put(SceneName.MENU, loadFxml("GameScreen"));
+    scenes.put(SceneName.GAMEBOARD, loadFxml("GameBoard"));
+    // BYPASSING MENU FOR TESTING
+    Parent root = scenes.get(SceneName.GAMEBOARD);
+    scene = new Scene(root, 1400, 800);
+    stage.setScene(scene);
+    stage.setTitle("Stacked Success");
+    stage.show();
+    root.requestFocus();
+
+    stage.setOnCloseRequest(
+        event -> {
+          System.exit(0);
         });
-    }
-
+  }
 }
